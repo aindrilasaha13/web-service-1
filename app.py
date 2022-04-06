9529487f61a8c28139f6b532c3b5aad6c403f243
Aindrila <aindrila.saha.kgec@gmail.com>
2022-04-06 11:35:21 +0530

    updated get post methods

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/",methods=["GET","POST"])
 def send():
 
-    x = [1,2,3,4,5]
-    y = [1,4,9,16,25]
+    N = int(request.args.get("N"))
+    x = list(range(1,N+1))
+    y = [i*i for i in range(1,N+1)]
     
     plt.title("squares")
     plt.plot(x, y, color='red')
@@ -22,4 +23,4 @@ def send():
     return send_file(img,as_attachment=True,mimetype='image/jpg') 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
